58d9676b27c522dc0e884550dd3b815cff969ad8
webtunings <webtunings@gmail.com>
2014-04-27 15:42:26 +0530

    Babylonian method for square root

---
+++ sqrt.java
@@ -0,0 +1,26 @@
+public class sqrt {
+
+ public static void main(String[] args) {
+ double result = getSqrt(Double.parseDouble(args[0]));
+ System.out.println("Answer: " + result);
+
+
+ }
+
+ public static double getSqrt(double number) {
+
+ double error = 1e-3;
+ double s = number;
+ 
+ while((s - number/s) > error) {
+ s = (s + number/s)/2;
+ System.out.println(s);
+ System.out.println("Current Error : " + (s - number/s));
+ }
+ System.out.println("Current Error : " + (s - number/s)); 
+ 
+ return s;
+ }
+
+}
+
