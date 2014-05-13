afad71fc6a604501ddb0cb44e9872466e66115f7
webtunings <webtunings@gmail.com>
2014-05-13 20:31:54 +0530

    coupon collector simulation

--- CouponCollectorSimulate.java
+++ CouponCollectorSimulate.java
@@ -1,33 +1,25 @@
 public class CouponCollectorSimulate {
 
  public static void main(String[] args) {
-  int total = 0, result = 0;
-  for(int i=0; i <50; i++) {
+  int result =0;
   result = couponCollector(Integer.parseInt(args[0]));
-  total += result;
   System.out.println(result);
-  }
-  System.out.println("Average=" + total/50);
-  }
-
-
+ }
 
  public static int couponCollector(int number) {
   boolean[] collected = new boolean[number];
   int iteration = 0;
   int unique = 0;
 
-  while(unique < number) {
+  while(unique < number){
    int random = (int) (Math.random() * number);
    if(!collected[random]){
     unique++;
     collected[random] = true;
    }
-   iteration++; 
+  iteration++;
   }
-
- return iteration;
+  return iteration;
  }
-
 
 }
